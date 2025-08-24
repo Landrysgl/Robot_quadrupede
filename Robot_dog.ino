b5b25d98a8e5d22180e204660865924f7afb6ecb
soglo <soglolandry06@gmail.com>
2025-08-24 15:59:32 +0200

    Ajout du code du robot quadrupède

---
+++ Robot_dog.ino
@@ -0,0 +1,147 @@
+#include <MPU6050.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+#include <math.h>
+#define SERVO_MIN 150 
+#define SERVO_MAX 600 
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+typedef struct test{
+    float J2;
+    float J3;
+}test;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pwm.begin();
+  pwm.setPWMFreq(50);
+}
+
+
+double radToDeg(double rad) {
+    return (rad*180) / M_PI;
+}
+
+void setServoAngle(uint8_t servoNum, float angleMin, float angle) {
+  if (angle < 0) angle = 0;
+  if (angle > 180) angle = 180;
+
+  int pulse = map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
+
+
+  pwm.setPWM(servoNum, angleMin, pulse);
+}
+
+
+
+test avancePre(float Y, float Z){
+test joueur;
+float L;
+float J3L = 12.5;
+float B;
+float J2L = 8;
+float A;
+
+L = sqrt((Y+90) * (Y+90) + (Z-60) * (Z-60));
+
+float valJ3 = acos((J2L * J2L + J3L * J3L - L * L) / (2 * J2L * J3L));
+
+char nan = "nan";
+
+if (valJ3 > 1.0) valJ3 = 1.0;
+if (valJ3 < -1.0) valJ3 = -1.0;
+if (valJ3 = nan) valJ3 = 0.5;
+
+joueur.J3 = radToDeg(valJ3);
+
+
+float valB = (J2L * J2L - J3L * J3L + L * L) / (2 * J2L * L);
+
+if (valB > 1.0) valB = 1.0;
+if (valB < -1.0) valB = -1.0;
+
+B = radToDeg(acos(valB));
+
+A = radToDeg(atan((Z-60) / (Y+90)));
+
+
+joueur.J2 = B - A;
+
+joueur.J2 = 90 - joueur.J2;
+
+return joueur;
+}
+
+void setAv(int servo1, int servo2, float Y, float Z){
+  
+  test angles = avancePre(Y,Z);
+
+
+if (servo1 == 4) {
+    angles.J3 = 60 + angles.J3;
+    angles.J2 = angles.J2 - 60;
+}
+
+  double angle1 = angles.J2;
+  double angle2 = angles.J3;
+}
+
+
+void avancer(){
+  setServoAngle(9, 0, 70);
+  setServoAngle(6, 0, 75); 
+  setServoAngle(3, 0, 73);
+  setServoAngle(0, 0, 170);
+
+  setServoAngle(2,0, 60);
+  setServoAngle(1,0, 70); 
+  setServoAngle(8,0, 0);
+  setServoAngle(7,0, 120);
+  delay(3000);
+  setServoAngle(2,0, 0);
+  setServoAngle(1,0, 20);  
+  setServoAngle(8,0, 120);
+  setServoAngle(7,0, 80);
+
+  setServoAngle(11,0, 0);
+  setServoAngle(10,0, 90); 
+  setServoAngle(5,0, 90);
+  setServoAngle(4,0, 150);
+  delay(3000);
+  setServoAngle(11,0, 80);
+  setServoAngle(10,0, 140);  
+  setServoAngle(5,0, 20);
+  setServoAngle(4,0, 100);
+};
+
+void pause(){
+  setServoAngle(2,0, 90);
+  setServoAngle(1,0, 70); 
+  setServoAngle(8,0, 90);
+  setServoAngle(7,0, 120);
+  setServoAngle(11,0, 20);
+  setServoAngle(10,0, 90); 
+  setServoAngle(5,0, 90);
+  setServoAngle(4,0, 150);  
+  
+  setServoAngle(9, 0, 55);
+  setServoAngle(6, 0, 75); 
+  setServoAngle(3, 0, 73);
+  setServoAngle(0, 0, 170);
+}
+
+
+
+void loop() {
+setAv(10, 11, -72,80);
+setAv(7, 8, 0,42);
+setAv(4, 5, -75, 100);
+setAv(2, 1, 10, 0);
+delay(4000); 
+pause();
+delay(4000);
+
+}
